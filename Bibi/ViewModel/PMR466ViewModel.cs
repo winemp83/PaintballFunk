909876320cbd05860f0856af9de28f90ac7e9386
Sascha Köhne <koehne83@googlemail.com>
2022-03-23 03:23:06 +0100

    Add basic PMR466 ViewModel

---
+++ Bibi/ViewModel/PMR466ViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibi.ViewModel
+{
+    public class PMR466ViewModel : BaseViewModel<Model.PMR466Model>
+    {
+        public override void Load()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
